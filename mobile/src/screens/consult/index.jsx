c77ad816f412e3df9dd83b4e8347ab31381dbb5e
MarioBatalha <mariobatalha9@gmail.com>
2023-03-26 17:28:28 +0100

    feat: add consult scheduling

--- mobile/src/screens/consult/index.jsx
+++ mobile/src/screens/consult/index.jsx
@@ -1,49 +1,60 @@
-import React, { useEffect } from "react";
-import { View, Text, Button, Platform } from "react-native";
-import * as Calendar from "expo-calendar";
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity, Platform } from "react-native";
+import CalendarPicker from "react-native-calendar-picker";
 import { styles } from "../../../style";
 
 export const Consult = () => {
-  useEffect(() => {
-    (async () => {
-      const { status } = await Calendar.requestCalendarPermissionsAsync();
-      if (status === "granted") {
-        const calendars = await Calendar.getCalendarsAsync(
-          Calendar.EntityTypes.EVENT
-        );
-        console.log("Here are all your calendars:");
-        console.log({ calendars });
-      }
-    })();
-  }, []);
+	const [selectDate, setSelectDate] = useState([]);
+	const startDate = selectDate ? selectDate.toString() : "";
 
-  return (
-    <View style={styles.consultContainer}>
-      <Text style={styles.consultTitle}>Agendar consulta</Text>
-      <Button title="Marcar no calendário" onPress={createCalendar} />
-    </View>
-  );
+	const handleChangeDate = (date) => {
+		setSelectDate((prevDate) => [date]);
+	};
+
+	const handleScheduleConsult = () => {
+		Alert.alert("consulta agendada");
+	};
+	return (
+		<View style={styles.consultContainer}>
+			<Text style={styles.consultTitle}>Agendar consulta</Text>
+			<CalendarPicker onDateChange={handleChangeDate} />
+
+			<View style={styles.content}>
+				<Text style={styles.subtitle}>
+					Data de consulta:
+					{startDate}
+				</Text>
+			</View>
+			<TouchableOpacity
+				style={styles.paymentButton}
+				onPress={handleScheduleConsult}
+				activeOpacity={0.7}
+			>
+				<Text style={styles.paymentTouchableValue}>Concluír agendamento</Text>
+			</TouchableOpacity>
+		</View>
+	);
 };
 
 const getDefaultCalendarSource = async () => {
-  const defaultCalendar = await Calendar.getDefaultCalendarAsync();
-  return defaultCalendar.source;
+	const defaultCalendar = await Calendar.getDefaultCalendarAsync();
+	return defaultCalendar.source;
 };
 
 const createCalendar = async () => {
-  const defaultCalendarSource =
-    Platform.OS === "ios"
-      ? await getDefaultCalendarSource()
-      : { isLocalAccount: true, name: "Expo Calendar" };
-  const newCalendarID = await Calendar.createCalendarAsync({
-    title: "Expo Calendar",
-    color: "blue",
-    entityType: Calendar.EntityTypes.EVENT,
-    sourceId: defaultCalendarSource.id,
-    source: defaultCalendarSource,
-    name: "internalCalendarName",
-    ownerAccount: "personal",
-    accessLevel: Calendar.CalendarAccessLevel.OWNER,
-  });
-  console.log(`Your new calendar ID is: ${newCalendarID}`);
+	const defaultCalendarSource =
+		Platform.OS === "ios"
+			? await getDefaultCalendarSource()
+			: { isLocalAccount: true, name: "Expo Calendar" };
+	const newCalendarID = await Calendar.createCalendarAsync({
+		title: "Expo Calendar",
+		color: "blue",
+		entityType: Calendar.EntityTypes.EVENT,
+		sourceId: defaultCalendarSource.id,
+		source: defaultCalendarSource,
+		name: "internalCalendarName",
+		ownerAccount: "personal",
+		accessLevel: Calendar.CalendarAccessLevel.OWNER,
+	});
+	console.log(`Your new calendar ID is: ${newCalendarID}`);
 };
